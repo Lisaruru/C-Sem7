5cae069cb47949e0f1b70abb0366dc9d537a885d
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-28 19:52:28 +0800

    add exs52

---
+++ exs52/Program.cs
@@ -0,0 +1,68 @@
+﻿//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+using System;
+
+namespace exs52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows=int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int columns=int.Parse(Console.ReadLine());
+            
+            int [,] array =GetArray(rows,columns,0,20);
+            Console.WriteLine();
+            PrintArray(array);   
+            ArithmeticMeanColumns(array);
+            int[,] GetArray(int rows, int columns, int min, int max)
+            {
+            int[,]result=new int[rows,columns];
+            Random random = new Random();
+            for (int i=0;i<rows;i++)
+                {
+                    for( int j=0; j<columns; j++)
+                    {
+                       result[i,j]= new Random().Next(min,max);
+                    }
+                }
+                return result;
+            }
+   
+            void PrintArray(int[,]array)
+            {     
+                for (int i=0;i<array.GetLength(0);i++)
+                {
+                    for( int j=0; j<array.GetLength(1); j++)
+                    {
+                        Console.Write($" {array[i,j]}");
+                    }
+                    Console.WriteLine();
+                }
+            } 
+             void ArithmeticMeanColumns(int[,]array)
+            {   
+                
+                for (int j=0; j<array.GetLength(1); j++)
+                {
+                     double arith=0;
+                     double sum=0;
+                    for( int i=0; i<array.GetLength(0); i++)
+                    {
+                    sum+= array[i,j];
+                    }
+                     arith=sum/array.GetLength(0);
+                     
+                    Console.WriteLine ($"среднее арифметическое столбца={arith} ");
+                } 
+            
+            } 
+        }   
+
+    }
+}
+
+            
+            
